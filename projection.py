2ccc52e5386071fd4f1ebaf3d7d485e667ad145b
arvberg <arvidbmobil@gmail.com>
2025-02-11 15:03:41 +0100

    Update projection.py

--- projection.py
+++ projection.py
@@ -17,8 +17,12 @@ def point_projection(p, v1, v2, n=None):
     result = n * ((dot(p, n)) / pow(abs(linalg.norm(n)), 2))
     return print(f"The point-projection is: {result}")
 
+# The two calculations below will assume the same value as each other.
+#  All values will be non-negative, so -1 does not matter over 1.
 point_projection([math.pi, math.e, 1], [1, 1, 0], [0, -1, 0])
 point_projection([-math.pi, math.e, 1], [1, 1, 0], [0, 1, 0])
 
+# The two calculations below will -also- assume the same value as each other.
+# The normal has the same direction in both, and assumes the same value.
 point_projection([math.pi, math.e, 1], None, None, [1, 1, 1])
 point_projection([-math.pi, math.e, 1], None, None, [3, 3, 3])
